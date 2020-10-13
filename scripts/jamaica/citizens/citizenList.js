b1e0460a2d641fdd7d85b79dad607fb9c46b0fb4
Travis Stevenson <thetechtravis@outlook.com>
2020-10-13 12:24:24 -0500

    Jamaica cities data has been successfully generated dynamically.

--- scripts/jamaica/citizens/citizenList.js
+++ scripts/jamaica/citizens/citizenList.js
@@ -1,8 +1,8 @@
 import { citizenHTMLLine } from "./citizen.js";
-import { useCitizen } from "./citizenDataProvider";
+import { useCitizen } from "./citizenDataProvider.js";
 
 export const citizenList = () => {
-    const contentElement = document.querySelector(".list-of-landmarks")
+    const contentElement = document.querySelector(".list-of-famous-citizens")
     
     const citizensCopy = useCitizen()
 
--- scripts/jamaica/main.js
+++ scripts/jamaica/main.js
@@ -1,5 +1,9 @@
 import { useCity } from "./cities/cityDataProvider.js"
 import { cityList } from "./cities/cityList.js"
+import { useCitizen } from "./citizens/citizenDataProvider.js"
+import { citizenList } from "./citizens/citizenList.js"
 
 useCity()
 cityList()
+useCitizen()
+citizenList()
