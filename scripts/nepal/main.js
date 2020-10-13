e3eda86b339a98070669ba80dc9b9bde70f9fd21
Benjamin Schweizer <bschweiz@gmail.com>
2020-10-13 12:08:39 -0500

    added fucntionality of the citizens modules to populate the famous citizens page

--- scripts/nepal/main.js
+++ scripts/nepal/main.js
@@ -4,6 +4,11 @@ import { cityList } from './cities/cityList.js'
 import { useLandmark } from "./landmarks/landmarkDataProvider.js"
 import { landmarkList } from './landmarks/landmarkList.js'
 
+import { useCitizen } from "./citizens/citizenDataProvider.js"
+import { citizenList } from './citizens/citizenList.js'
+
+useCitizen()
+citizenList()
 
 useLandmark()
 landmarkList()
